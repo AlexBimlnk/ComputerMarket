d96023babffd48855adaf1a36a1de76fc0155d8f
centuriin <centuriin@yandex.ru>
2022-09-23 19:55:30 +0300

    #2 added unit tests proj to import

--- Import.Logic/Product.cs
+++ Import.Logic/Product.cs
@@ -5,14 +5,13 @@
 /// </summary>
 public sealed class Product
 {
+    private InternalID? _internalID;
+
     /// <summary xml:lang = "ru">
     /// Создает новый экземпляр типа <see cref="Product"/>
     /// </summary>
     /// <param name="externalID" xml:lang = "ru">
     /// Внешний идентификатор продукта.
-    /// </param>
-    /// <param name="internalID" xml:lang = "ru">
-    /// Внутренний идентификатор продукта.
     /// </param>
     /// <param name="price" xml:lang = "ru">
     /// Цена.
@@ -20,17 +19,18 @@ public sealed class Product
     /// <param name="quantity" xml:lang = "ru">
     /// Количество.
     /// </param>
+    /// <param name="internalID" xml:lang = "ru">
+    /// Внутренний идентификатор продукта.
+    /// </param>
     /// <exception cref="ArgumentOutOfRangeException" xml:lang = "ru">
     /// Когда <paramref name="quantity"/> меньше нуля.
     /// </exception>
     public Product(
         ExternalID externalID, 
-        InternalID? internalID, 
         Price price, 
         int quantity)
     {
         ExternalID = externalID;
-        InternalID = internalID;
         Price = price;
 
         if (quantity < 0)
@@ -47,7 +47,8 @@ public sealed class Product
     /// <summary xml:lang = "ru">
     /// Внутренний идентификатор продукта.
     /// </summary>
-    public InternalID? InternalID { get; }
+    public InternalID InternalID =>
+        _internalID ?? throw new InvalidOperationException("Product isn't mapped.");
 
     /// <summary xml:lang = "ru">
     /// Цена.
@@ -58,4 +59,24 @@ public sealed class Product
     /// Количество.
     /// </summary>
     public int Quantity { get; }
+
+    /// <summary xml:lang = "ru">
+    /// Указывает на наличие связи с внутренним продуктом.
+    /// </summary>
+    public bool IsMapped { get; private set; }
+
+    /// <summary xml:lang = "ru">
+    /// Устанавливает связь с внутренним продуктом.
+    /// </summary>
+    /// <param name="internalID" xml:lang = "ru">
+    /// Идентификатор внутреннего продукта.
+    /// </param>
+    public void MapTo(InternalID internalID)
+    {
+        if (IsMapped)
+            throw new InvalidOperationException("Product already is mapped.");
+
+        _internalID = internalID;
+        IsMapped = true;
+    }
 }
