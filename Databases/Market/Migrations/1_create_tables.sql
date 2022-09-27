035a86040925d47951d5d823dd6897877376ba49
Semeon Bogdan <semen2002bs@gmail.com>
2022-09-27 09:50:11 +0300

    #7 item description tables

--- Databases/Market/Migrations/1_create_tables.sql
+++ Databases/Market/Migrations/1_create_tables.sql
@@ -1,7 +1,8 @@
-
 begin;
 
+DROP TABLE IF EXISTS item_description;
 DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS type_description;
 DROP TABLE IF EXISTS item_type;
 DROP TABLE IF EXISTS item_properties;
 DROP TABLE IF EXISTS property_group;
@@ -12,10 +13,10 @@ CREATE TABLE property_group(Id int primary key, Name varchar(20));
 CREATE TABLE item_properties(
     Id int primary key,
     Name varchar(20) not null,
-    Group_Id int null default null,
-        foreign key (Group_Id) references property_group(Id),
-    Is_Filterable bool not null default false,
-    PropertyDataType varchar(10) not null default 'varchar'
+    Group_id int null default null,
+        foreign key (Group_id) references property_group(Id),
+    Is_filterable bool not null default false,
+    Property_data_type varchar(10) not null default 'varchar'
     );
 
 CREATE TABLE item_type(
@@ -26,9 +27,24 @@ CREATE TABLE item_type(
 CREATE TABLE items(
     Id int primary key,
     Name varchar(40),
-    Type_Id int not null ,
-        foreign key (Type_Id) references item_type(Id)
+    Type_id int not null ,
+        foreign key (Type_id) references item_type(Id)
 
+);
+
+CREATE TABLE type_description(
+    Type_id int not null,
+        foreign key (Type_id) references item_type(Id),
+    Property_id int not null,
+        foreign key (Property_id) references item_properties(Id)
+);
+
+CREATE TABLE item_description(
+    Item_id int not null,
+        foreign key (Item_id) references items(Id),
+    Property_Id int not null,
+        foreign key (Property_Id) references item_properties(Id),
+    Property_value varchar(40) null default null
 );
 
 
