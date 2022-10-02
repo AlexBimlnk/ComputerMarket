6abbf52c85da125e9afae5ebec70f2e8016b7808
centuriin <centuriin@yandex.ru>
2022-10-02 20:57:11 +0300

    #27 units

--- Import.Logic.Tests/Storage/LinkRepository.Tests.cs
+++ Import.Logic.Tests/Storage/LinkRepository.Tests.cs
@@ -71,7 +71,7 @@ public class LinkRepositoryTests
         var linksCallback = 0;
         links.Setup(x => x
             .AddAsync(
-                It.Is<TLink>(l => 
+                It.Is<TLink>(l =>
                     l.InternalId == storageLink.InternalId &&
                     l.ExternalId == storageLink.ExternalId &&
                     l.ProviderId == storageLink.ProviderId &&
@@ -92,7 +92,7 @@ public class LinkRepositoryTests
             new(1, Provider.Ivanov));
 
         // Act
-        var exception = await Record.ExceptionAsync(async () => 
+        var exception = await Record.ExceptionAsync(async () =>
             await linkRepository.AddAsync(inputLink));
 
         // Assert
@@ -100,5 +100,193 @@ public class LinkRepositoryTests
         linksCallback.Should().Be(1);
     }
 
-    
+    [Fact(DisplayName = $"The {nameof(LinkRepository)} cannot add link when link is null.")]
+    [Trait("Category", "Unit")]
+    public async void CanNotAddLinkWhenLinkIsNullAsync()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger>();
+
+        var linkRepository = new LinkRepository(
+            context.Object,
+            logger);
+
+        // Act
+        var exception = await Record.ExceptionAsync(async () =>
+            await linkRepository.AddAsync(null!));
+
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(LinkRepository)} can cancel add link.")]
+    [Trait("Category", "Unit")]
+    public async void CanCancelAddLinkAsync()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger>();
+
+        var cts = new CancellationTokenSource();
+
+        var linkRepository = new LinkRepository(
+            context.Object,
+            logger);
+
+        var inputLink = new Link(
+            new(1),
+            new(1, Provider.Ivanov));
+
+        // Act
+        cts.Cancel();
+        var exception = await Record.ExceptionAsync(async () =>
+            await linkRepository.AddAsync(inputLink, cts.Token));
+
+        // Assert
+        exception.Should().BeOfType<OperationCanceledException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(LinkRepository)} cannot contains link when link is null.")]
+    [Trait("Category", "Unit")]
+    public async void CanNotContainsWhenLinkIsNullAsync()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger>();
+
+        var linkRepository = new LinkRepository(
+            context.Object,
+            logger);
+
+        // Act
+        var exception = await Record.ExceptionAsync(async () =>
+            await linkRepository.ContainsAsync(null!));
+
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(LinkRepository)} can cancel contains link.")]
+    [Trait("Category", "Unit")]
+    public async void CanCancelContainsAsync()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger>();
+
+        var cts = new CancellationTokenSource();
+
+        var linkRepository = new LinkRepository(
+            context.Object,
+            logger);
+
+        var inputLink = new Link(
+            new(1),
+            new(1, Provider.Ivanov));
+
+        // Act
+        cts.Cancel();
+        var exception = await Record.ExceptionAsync(async () =>
+            await linkRepository.ContainsAsync(inputLink, cts.Token));
+
+        // Assert
+        exception.Should().BeOfType<OperationCanceledException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(LinkRepository)} can delete link.")]
+    [Trait("Category", "Unit")]
+    public void CanDeleteLink()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger>();
+
+        var storageLink = new TLink()
+        {
+            InternalId = 1,
+            ExternalId = 1,
+            ProviderId = 1
+        };
+
+        var links = new Mock<DbSet<TLink>>(MockBehavior.Loose);
+
+        context.Setup(x => x.Links)
+            .Returns(links.Object);
+
+        var linkRepository = new LinkRepository(
+            context.Object,
+            logger);
+
+        var containsLink = new Link(
+            new(1),
+            new(1, Provider.Ivanov));
+
+        // Act
+        var exception = Record.Exception(() =>
+            linkRepository.Delete(containsLink));
+
+        // Assert
+        exception.Should().BeNull();
+
+        links.Verify(x =>
+            x.Remove(
+                It.Is<TLink>(l =>
+                    l.InternalId == storageLink.InternalId &&
+                    l.ExternalId == storageLink.ExternalId &&
+                    l.ProviderId == storageLink.ProviderId &&
+                    l.Provider == storageLink.Provider)),
+            Times.Once);
+    }
+
+    [Fact(DisplayName = $"The {nameof(LinkRepository)} cannot delete link when link is null.")]
+    [Trait("Category", "Unit")]
+    public void CanNotDeleteWhenLinkIsNull()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger>();
+
+        var linkRepository = new LinkRepository(
+            context.Object,
+            logger);
+
+        // Act
+        var exception = Record.Exception(() =>
+            linkRepository.Delete(null!));
+
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(LinkRepository)} can save.")]
+    [Trait("Category", "Unit")]
+    public void CanSave()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Loose);
+        var logger = Mock.Of<ILogger>();
+
+        var storageLink = new TLink()
+        {
+            InternalId = 1,
+            ExternalId = 1,
+            ProviderId = 1
+        };
+
+        var linkRepository = new LinkRepository(
+            context.Object,
+            logger);
+
+        // Act
+        var exception = Record.Exception(() =>
+            linkRepository.Save());
+
+        // Assert
+        exception.Should().BeNull();
+
+        context.Verify(x =>
+            x.SaveChanges(),
+            Times.Once);
+    }
 }
