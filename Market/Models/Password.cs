7ccb8bda7b694f04625961ffb68b768ac786a2e3
Semeon Bogdan <semen2002bs@gmail.com>
2022-10-04 21:29:08 +0300

    #10 add inn pswd provider meta data

---
+++ Market/Models/Password.cs
@@ -0,0 +1,25 @@
+﻿namespace Market.Models;
+
+/// <summary xml:lang = "ru">
+/// Представляет пароль пользователя.
+/// </summary>
+public class Password
+{
+    /// <summary xml:lang = "ru">
+    /// Значение.
+    /// </summary>
+    public string Value { get; private set; }
+
+    /// <summary xml:lang = "ru">
+    /// Создает экземпляр типа <see cref="Password"/>.
+    /// </summary>
+    /// <param name="value" xml:lang = "ru">Пароль пользователя.</param>
+    /// <exception cref="ArgumentException" xml:lang = "ru">Если <paramref name="value"/> имеет неверный формат.</exception>
+    public Password(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("Password can't be null or empty or contains only whitespaces", nameof(value));
+
+        Value = value;
+    }
+}
