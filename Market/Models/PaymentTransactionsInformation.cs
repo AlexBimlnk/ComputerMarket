72dfc108dec8e70c641f8c7050f9df12a43dcba5
Semeon Bogdan <semen2002bs@gmail.com>
2022-10-06 00:51:25 +0300

    #10 model fixes

--- Market/Models/PaymentTransactionsInformation.cs
+++ Market/Models/PaymentTransactionsInformation.cs
@@ -1,14 +1,18 @@
-﻿namespace Market.Models;
+﻿using System.Text.RegularExpressions;
+
+namespace Market.Models;
 
 /// <summary xml:lang = "ru">
 /// Представляет информацию для платежных операций.
 /// </summary>
 public class PaymentTransactionsInformation
 {
+    private readonly static string s_innPattern = @"^[0-9]{10}$";
+
     /// <summary xml:lang = "ru">
     /// Инн поставщика.
     /// </summary>
-    public INN INN { get; private set; }
+    public string INN { get; private set; }
 
     /// <summary xml:lang = "ru">
     /// Банковский счёт поставщика.
@@ -20,13 +24,22 @@ public class PaymentTransactionsInformation
     /// </summary>
     /// <param name="inn" xml:lang = "ru">Инн поставщика.</param>
     /// <param name="bankAccount" xml:lang = "ru">Счёт поставщика.</param>
-    /// <exception cref="ArgumentException" xml:lang = "ru">Если <paramref name="bankAccount"/> имеет неверный формат.</exception>
-    public PaymentTransactionsInformation(INN inn, string bankAccount)
+    /// <exception cref="ArgumentException" xml:lang = "ru">
+    /// Если <paramref name="bankAccount"/> или <paramref name="inn"/> имеет неверный формат.
+    /// </exception>
+    public PaymentTransactionsInformation(string inn, string bankAccount)
     {
         if (string.IsNullOrWhiteSpace(bankAccount))
             throw new ArgumentException("Bank account can't be null or empty or contains only whitespaces", nameof(bankAccount));
 
         BankAccount = bankAccount;
+
+        if (string.IsNullOrWhiteSpace(inn))
+            throw new ArgumentException("INN can't be null or empty or contains only whitespaces", nameof(inn));
+
+        if (!Regex.IsMatch(inn, s_innPattern))
+            throw new ArgumentException("Given inn not match with INN format");
+
         INN = inn;
     }
 }
