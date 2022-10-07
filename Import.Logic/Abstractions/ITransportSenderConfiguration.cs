4da44af1aa0a9f7d0678b25d47560c2ba6e8e805
centuriin <centuriin@yandex.ru>
2022-10-07 21:53:22 +0300

    #44 add abstrations

---
+++ Import.Logic/Abstractions/ITransportSenderConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace Import.Logic.Abstractions;
+
+/// <summary xml:lang = "ru">
+/// Описывает транспортную конфигурацию отправителя.
+/// </summary>
+public interface ITransportSenderConfiguration
+{
+    /// <summary xml:lang = "ru">
+    /// Место назначения.
+    /// </summary>
+    public string Destination { get; }
+}
