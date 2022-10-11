c34e38b123f04593e5707f234d6a9c0e86100d98
Semeon Bogdan <semen2002bs@gmail.com>
2022-10-11 09:03:20 +0300

    #20 first realization

---
+++ Import.Logic/Cache.cs
@@ -0,0 +1,44 @@
+﻿using Import.Logic.Abstractions;
+using Import.Logic.Models;
+
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Import.Logic;
+public sealed class Cache : IKeyableCache<Link, ExternalID>
+{
+    private readonly Dictionary<ExternalID, Link> _dictionaryCache;
+
+    public Cache()
+    {
+        _dictionaryCache = new Dictionary<ExternalID, Link>();
+    }
+
+    public void Add(Link entity)
+    {
+        if (!_dictionaryCache.ContainsKey(entity.ExternalID))
+        {
+            _dictionaryCache[entity.ExternalID] = entity;
+        }
+    }
+
+    public void AddRange(IEnumerable<Link> entities)
+    {
+        foreach(var entity in entities)
+        {
+            Add(entity);
+        }
+    }
+
+    public bool Contains(ExternalID key) => _dictionaryCache.ContainsKey(key);
+
+    public bool Contains(Link entity) => _dictionaryCache.ContainsKey(entity.ExternalID);
+    public void Delete(Link entity) => _dictionaryCache.Remove(entity.ExternalID);
+    public Link? GetByKey(ExternalID key)
+    {
+        Link? returnable;
+
+        _ = _dictionaryCache.TryGetValue(key, out returnable);
+
+        return returnable;
+    }
+}
