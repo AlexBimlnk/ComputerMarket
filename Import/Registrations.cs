8358f98b7d11f86e3f51185c096c1be9fbc0b478
centuriin <centuriin@yandex.ru>
2022-10-13 22:34:17 +0300

    #60 resolve DI

--- Import/Registrations.cs
+++ Import/Registrations.cs
@@ -1,13 +1,22 @@
-﻿using Import.Logic.Abstractions.Commands;
+﻿using Import.Logic;
+using Import.Logic.Abstractions.Commands;
 using Import.Logic.Commands;
 
 namespace Import;
 
 public static class Registrations
 {
-    public static IServiceCollection AddLogic(this IServiceCollection services) 
+    public static IServiceCollection AddImportServices(this IServiceCollection services)
+        => services
+            .AddHostedServices()
+            .AddLogic();
+
+    private static IServiceCollection AddLogic(this IServiceCollection services) 
         => services
             .AddSingleton<ICommandFactory, CommandFactory>()
             .AddSingleton<Func<SetLinkCommandParameters, ICommand>>(
-                provider => (parameters) => ActivatorUtilities.CreateInstance<SetLinkCommand>(provider, parameters));
+               static provider => (parameters) => ActivatorUtilities.CreateInstance<SetLinkCommand>(provider, parameters));
+
+    private static IServiceCollection AddHostedServices(this IServiceCollection services)
+        => services.AddHostedService<CacheInizializerService>();
 }
