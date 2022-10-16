e56e07d85fce02c780f042a8f31de0b993b0594b
Semeon Bogdan <semen2002bs@gmail.com>
2022-10-16 10:05:17 +0300

    #55 product

---
+++ Market.Logic/Models/Price.cs
@@ -0,0 +1,29 @@
+﻿namespace Market.Logic.Models;
+
+/// <summary xml:lang = "ru">
+/// Представляет цену продукта.
+/// </summary>
+public struct Price
+{
+    /// <summary xml:lang = "ru">
+    /// Создает новый экемпляр типа <see cref="ProductPrice"/>.
+    /// </summary>
+    /// <param name="price" xml:lang = "ru">
+    /// Цена продукта.
+    /// </param>
+    /// <exception cref="ArgumentOutOfRangeException" xml:lang = "ru">
+    /// Если <paramref name="price"/> меньше или равен нулю.
+    /// </exception>
+    public Price(decimal price)
+    {
+        if (price <= 0)
+            throw new ArgumentOutOfRangeException(nameof(price));
+
+        Value = price;
+    }
+
+    /// <summary xml:lang = "ru">
+    /// Значение.
+    /// </summary>
+    public decimal Value { get; private set; }
+}
---
+++ Market.Logic/Models/Product.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Logic.Models;
+
+/// <summary xml:lang = "ru">
+/// Продукт.
+/// </summary>
+public sealed class Product
+{
+    private readonly Price _price;
+
+    /// <summary xml:lang = "ru">
+    /// Создает экземпляр типа <see cref="Product"/>.
+    /// </summary>
+    /// <param name="item" xml:lang = "ru">Описание продукта.</param>
+    /// <param name="provider" xml:lang = "ru">Поставщик продукта.</param>
+    /// <param name="price" xml:lang = "ru">Цена назначенная поставщиком.</param>
+    /// <param name="quantiy" xml:lang = "ru">Колличество продукта.</param>
+    /// <exception cref="ArgumentNullException" xml:lang = "ru">
+    /// Если <paramref name="item"/> или <paramref name="price"/> - <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentOutOfRangeException" xml:lang = "ru">
+    /// Если <paramref name="quantiy"/> меньше 0.
+    /// </exception>
+    public Product(Item item, Provider provider, Price price, int quantiy)
+    {
+        Item = item ?? throw new ArgumentNullException(nameof(item));
+        _price = price;
+        Provider = provider ?? throw new ArgumentNullException(nameof(item));
+        
+        if (quantiy < 0)
+            throw new ArgumentOutOfRangeException(nameof(quantiy));
+        
+        Quantity = quantiy;
+    }
+
+    /// <summary xml:lang = "ru">
+    /// Описание продукта.
+    /// </summary>
+    public Item Item { get; }
+
+    /// <summary xml:lang = "ru">
+    /// Цена назначаенная поставщиком.
+    /// </summary>
+    public decimal ProviderCost => _price.Value;
+
+    /// <summary xml:lang = "ru">
+    /// Итоговая цена продукта.
+    /// </summary>
+    public decimal FinalCost => _price.Value * Provider.Margin;
+
+    /// <summary xml:lang = "ru">
+    /// Поставщик продукта.
+    /// </summary>
+    public Provider Provider { get; }
+
+    /// <summary xml:lang = "ru">
+    /// Колличестов продукта.
+    /// </summary>
+    public int Quantity { get; private set; }
+}
