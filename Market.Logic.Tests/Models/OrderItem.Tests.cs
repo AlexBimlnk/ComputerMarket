67936cc650d9d4730a27b56d44b04076fe960e90
w1sp <semen2002bs@gmail.com>
2022-10-18 13:52:42 +0300

    #55 order item changes

--- Market.Logic.Tests/Models/OrderItem.Tests.cs
+++ Market.Logic.Tests/Models/OrderItem.Tests.cs
@@ -1,4 +1,6 @@
-﻿using Market.Logic.Models;
+﻿using System.Reflection;
+
+using Market.Logic.Models;
 
 namespace Market.Logic.Tests.Models;
 
@@ -23,7 +25,7 @@ public class OrderItemTests
                     "0123456789",
                     "01234012340123401234")),
             price: new Price(100m),
-            quantity: 5);
+            quantity: quantity);
 
         // Act
         var exception = Record.Exception(() => item = new OrderItem(product, quantity));
@@ -32,6 +34,7 @@ public class OrderItemTests
         exception.Should().BeNull();
         item.Product.Should().Be(product);
         item.Quantity.Should().Be(quantity);
+        item.Selected.Should().BeTrue();
     }
 
     [Fact(DisplayName = $"The {nameof(OrderItem)} cannot be created without {nameof(Product)}.")]
@@ -45,10 +48,11 @@ public class OrderItemTests
         exception.Should().BeOfType<ArgumentNullException>();
     }
 
-    [Theory(DisplayName = $"The {nameof(OrderItem)} cannot be created with quantity less than one.")]
+    [Theory(DisplayName = $"The {nameof(OrderItem)} cannot be created with quantity less than one or greater than product quantity.")]
     [Trait("Category", "Unit")]
     [InlineData(0)]
     [InlineData(-1)]
+    [InlineData(10000000)]
     public void CanNotCreateWhenQuantityLessThanOne(int quantity)
     {
         // Arrange
@@ -71,5 +75,78 @@ public class OrderItemTests
 
         // Assert
         exception.Should().BeOfType<ArgumentOutOfRangeException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(OrderItem)} can increase product quantity.")]
+    [Trait("Category", "Unit")]
+    public void CanIncreaseQuantity()
+    {
+        // Arrange
+        var quantity = 1;
+        var product = new Product(
+            item: new Item(
+                new ItemType("some_type"),
+                "some_name",
+                properties: Array.Empty<ItemProperty>()),
+            provider: new Provider(
+                "provider_name",
+                new Margin(1.1m),
+                new PaymentTransactionsInformation(
+                    "0123456789",
+                    "01234012340123401234")),
+            price: new Price(100m),
+            quantity: 5);
+
+        var item = new OrderItem(product, quantity);
+        var expectedResult = 3;
+
+        // Act
+        item.IncQuantity();
+        item.IncQuantity();
+        var result = item.Quantity;
+        item.IncQuantity();
+        item.IncQuantity();
+        item.IncQuantity();
+        item.IncQuantity();
+
+        // Assert
+        result.Should().Be(expectedResult);
+        item.Quantity.Should().Be(product.Quantity);
+    }
+
+    [Fact(DisplayName = $"The {nameof(OrderItem)} can decrease product quantity.")]
+    [Trait("Category", "Unit")]
+    public void CanDecreaseQuantity()
+    {
+        // Arrange
+        var quantity = 4;
+        var product = new Product(
+            item: new Item(
+                new ItemType("some_type"),
+                "some_name",
+                properties: Array.Empty<ItemProperty>()),
+            provider: new Provider(
+                "provider_name",
+                new Margin(1.1m),
+                new PaymentTransactionsInformation(
+                    "0123456789",
+                    "01234012340123401234")),
+            price: new Price(100m),
+            quantity: 5);
+
+        var item = new OrderItem(product, quantity);
+        var expectedResult = 2;
+
+        // Act
+        item.DecQuantity();
+        item.DecQuantity();
+        var result = item.Quantity;
+        item.DecQuantity();
+        item.DecQuantity();
+        item.DecQuantity();
+
+        // Assert
+        result.Should().Be(expectedResult);
+        item.Quantity.Should().Be(1);
     }
 }
--- Market.Logic/Models/OrderItem.cs
+++ Market.Logic/Models/OrderItem.cs
@@ -3,7 +3,7 @@
 /// <summary xml:lang = "ru">
 /// Позиция в заказе.
 /// </summary>
-public sealed class OrderItem
+public sealed class OrderItem: IEquatable<OrderItem>
 {
     /// <summary xml:lang = "ru">
     /// Создает экземпляр типа <see cref="OrderItem"/>.
@@ -11,12 +11,12 @@ public sealed class OrderItem
     /// <param name="product"  xml:lang = "ru">Товар в позиции заказа.</param>
     /// <param name="quantity"  xml:lang = "ru">Колличесиво товар в позиции.</param>
     /// <exception cref="ArgumentNullException"  xml:lang = "ru">Если <paramref name="product"/> - <see langword="null"/>.</exception>
-    /// <exception cref="ArgumentOutOfRangeException"  xml:lang = "ru"> Если <paramref name="quantity"/> <= 0.</exception>
+    /// <exception cref="ArgumentOutOfRangeException"  xml:lang = "ru"> Если <paramref name="quantity"/> <= 0 или больше колличества продуктов.</exception>
     public OrderItem(Product product, int quantity)
     {
         Product = product ?? throw new ArgumentNullException(nameof(product));
         
-        if (quantity <= 0)
+        if (quantity < 1 || quantity > product.Quantity)
             throw new ArgumentOutOfRangeException(nameof(quantity));
 
         Quantity = quantity;
@@ -30,6 +30,41 @@ public sealed class OrderItem
     /// <summary  xml:lang = "ru">
     /// Количество продуктов.
     /// </summary>
-    public int Quantity { get; }
+    public int Quantity { get; private set; }
 
+    /// <summary xml:lang = "ru">
+    /// Выбран ли продукт.
+    /// </summary>
+    public bool Selected { get; set; } = true;
+
+    /// <summary xml:lang = "ru">
+    /// Увеличивает количество продуктов на один.
+    /// </summary>
+    public void IncQuantity()
+    {
+        if (Quantity == Product.Quantity)
+            return;
+
+        Quantity++;
+    }
+
+    /// <summary xml:lang = "ru">
+    /// Уменьшает количество продуктов на один до 1.
+    /// </summary>
+    public void DecQuantity()
+    {
+        if (Quantity == 1)
+            return;
+
+        Quantity--;
+    }
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => Product.GetHashCode();
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => obj is OrderItem item && Equals(item);
+
+    /// <inheritdoc/>
+    public bool Equals(OrderItem? other) => Product.Equals(other?.Product);
 }
