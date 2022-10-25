0e48c50491f5a023278ec9d74d3abc93f04143d2
w1sp <semen2002bs@gmail.com>
2022-10-25 11:16:05 +0300

    #68 product desirializer tests

---
+++ Market.Logic.Tests/Transport/Deserializers/ProductDeserializer.Tests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Market.Logic.Transport.Deserializers;
+using Market.Logic.Transport.Models;
+
+using Newtonsoft.Json;
+
+namespace Market.Logic.Tests.Transport.Deserializers;
+
+public class ProductDeserializerTests
+{
+    [Fact(DisplayName = $"The {nameof(ProductDeserializer)} can create.")]
+    [Trait("Category", "Unit")]
+    public void CanBeCreated()
+    {
+        // Act
+        var exception = Record.Exception(() =>
+            _ = new ProductDeserializer());
+
+        // Assert
+        exception.Should().BeNull();
+    }
+
+    [Theory(DisplayName = $"The {nameof(ProductDeserializer)} can deserialize.")]
+    [Trait("Category", "Unit")]
+    [MemberData(nameof(DeserializeParameters))]
+    public void CanDeserialize(string rawSource, ICollection<Product> expectedResult)
+    {
+        // Arrange
+        var deserializer = new ProductDeserializer();
+
+        // Act
+        var result = deserializer.Deserialize(rawSource);
+
+        // Assert
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+    [Theory(DisplayName = $"The {nameof(ProductDeserializer)} can't deserialize bad string.")]
+    [Trait("Category", "Unit")]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData("\r\n   \t ")]
+    public void CanNotDeserializeBadString(string source)
+    {
+        // Arrange
+        var deserializer = new ProductDeserializer();
+
+        // Act
+        var exception = Record.Exception(() =>
+            _ = deserializer.Deserialize(source));
+
+        // Assert
+        exception.Should().BeOfType<ArgumentException>();
+    }
+
+
+
+    public readonly static TheoryData<string, IReadOnlyCollection<Product>> DeserializeParameters = new()
+    {
+        {
+            /*lang=json,strict*/@"[
+                {""external_id"":1,""internal_id"":1,""provider_name"":""ivanov"",""price"":100.0,""quantity"":5},
+                {""external_id"":1,""internal_id"":4,""provider_name"":""horns_and_hooves"",""price"":55.0,""quantity"":3}
+            ]",
+            new []
+            {
+                new Product() {ExternalID = 1, InternalID = 1, Provider = "ivanov", Price = 100m, Quantity = 5 },
+                new Product() {ExternalID = 1, InternalID = 4, Provider = "horns_and_hooves", Price = 55m, Quantity = 3 }
+            }
+        },
+        {
+            /*lang=json,strict*/@"[]",
+            Array.Empty<Product>()
+        }
+    };
+}
