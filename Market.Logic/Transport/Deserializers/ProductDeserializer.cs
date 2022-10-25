34fab0a8acb66cbd41af24455c95a589009f3c49
w1sp <semen2002bs@gmail.com>
2022-10-25 08:55:37 +0300

    #68 product desirializer

---
+++ Market.Logic/Transport/Deserializers/ProductDeserializer.cs
@@ -0,0 +1,32 @@
+﻿using General.Transport;
+using Market.Logic.Transport.Models;
+
+using Newtonsoft.Json;
+
+namespace Market.Logic.Transport.Deserializers;
+
+/// <summary xml:lang = "ru">
+/// Дессериализатор из <see cref="string"/> в <see cref="Product"/>.
+/// </summary>
+public sealed class ProductDeserializer : IDeserializer<string, IReadOnlyCollection<Product>>
+{
+    private JsonSerializer _serializer = JsonSerializer.CreateDefault();
+
+    /// <inheritdoc/>
+    /// /// <exception cref="ArgumentException" xml:lang = "ru">
+    /// Когда <paramref name="source"/> имел неверный формат.
+    /// </exception>
+    public IReadOnlyCollection<Product> Deserialize(string source)
+    {
+        if (string.IsNullOrWhiteSpace(source))
+            throw new ArgumentException(
+                "The source is null, has only whitespaces or empty.",
+                nameof(source));
+
+        using var reader = new JsonTextReader(new StringReader(source));
+
+        var result = _serializer.Deserialize<IReadOnlyCollection<Product>>(reader)!;
+
+        return result;
+    }
+}
---
+++ Market.Logic/Transport/Model/Product.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace Market.Logic.Transport.Models;
+
+/// <summary xml:lang = "ru">
+/// Транспортная модель продукта.
+/// </summary>
+public sealed class Product
+{
+    /// <summary xml:lang = "ru">
+    /// Внешний индетификатор продукта.
+    /// </summary>
+    [JsonProperty("external_id")]
+    public long ExternalID { get; set; }
+
+    /// <summary xml:lang = "ru">
+    /// Внутрений индетификатор продукта.
+    /// </summary>
+    [JsonProperty("internal_id")]
+    public long? InternalID { get; set; }
+
+    /// <summary xml:lang = "ru">
+    /// Название поставщика.
+    /// </summary>
+    [JsonProperty("provider_name")]
+    public string Provider { get; set; }
+
+    /// <summary xml:lang = "ru">
+    /// Цена продукта.
+    /// </summary>
+    [JsonProperty("price")]
+    public decimal Price { get; set; }
+
+    /// <summary xml:lang = "ru">
+    /// Колличество продукта.
+    /// </summary>
+    [JsonProperty("quantity")]
+    public int Quantity { get; set; }
+}
