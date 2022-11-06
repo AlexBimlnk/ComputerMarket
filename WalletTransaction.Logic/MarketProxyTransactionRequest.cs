88d2aee0f2a68ae2a25f0aa2c1abe6bba5f8a7d1
centuriin <centuriin@yandex.ru>
2022-11-06 19:48:05 +0300

    #14 add proxy

---
+++ WalletTransaction.Logic/MarketProxyTransactionRequest.cs
@@ -0,0 +1,32 @@
+﻿namespace WalletTransaction.Logic;
+public class MarketProxyTransactionRequest : ITransactionsRequest
+{
+    private static readonly BankAccount s_marketAccount = new("01234012340123401234");
+
+    private readonly ITransactionsRequest _wrappedRequest;
+
+    public MarketProxyTransactionRequest(ITransactionsRequest transactionRequest)
+    {
+        _wrappedRequest = transactionRequest ?? throw new ArgumentNullException(nameof(transactionRequest));
+    }
+
+    public InternalID Id => _wrappedRequest.Id;
+
+    public IReadOnlyCollection<Transaction> Transactions => GetTransactions();
+
+    private IReadOnlyCollection<Transaction> GetTransactions()
+    {
+        var fromAccount = _wrappedRequest.Transactions
+            .Select(x => x.From)
+            .Distinct()
+            .Single();
+
+        var transferedBalace = _wrappedRequest.Transactions
+            .Select(x => x.TransferBalance)
+            .Sum();
+
+        var transaction = new Transaction(fromAccount, s_marketAccount, transferedBalace);
+
+        return new List<Transaction>(1) { transaction };
+    }
+}
