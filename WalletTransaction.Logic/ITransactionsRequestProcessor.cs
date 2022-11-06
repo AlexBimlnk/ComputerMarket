be65bcf6fc7bcae3ffbfb39b4830506c07ab8957
centuriin <centuriin@yandex.ru>
2022-11-06 19:48:13 +0300

    #14 add processor

---
+++ WalletTransaction.Logic/ITransactionsRequestProcessor.cs
@@ -0,0 +1,31 @@
+﻿namespace WalletTransaction.Logic;
+
+/// <summary xml:lang = "ru">
+/// Описывает обработчика запросов на выполнение транзакций.
+/// </summary>
+public interface ITransactionsRequestProcessor
+{
+    /// <summary xml:lang = "ru">
+    /// Обработка запроса.
+    /// </summary>
+    /// <param name="transactionRequest" xml:lang = "ru">
+    /// Запрос на обработку транзакций.
+    /// </param>
+    /// <param name="token" xml:lang = "ru">
+    /// Токен отмены.
+    /// </param>
+    /// <returns xml:lang = "ru"> <see cref="Task"/>. </returns>
+    public Task ProcessAsync(ITransactionsRequest transactionRequest, CancellationToken token);
+
+    /// <summary xml:lang = "ru">
+    /// Возврат денежных средств по запросу.
+    /// </summary>
+    /// <param name="transactionRequest" xml:lang = "ru">
+    /// Запрос на обработку транзакций.
+    /// </param>
+    /// <param name="token" xml:lang = "ru">
+    /// Токен отмены.
+    /// </param>
+    /// <returns xml:lang = "ru"> <see cref="Task"/>. </returns>
+    public Task RefundAsync(ITransactionsRequest transactionRequest, CancellationToken token);
+}
---
+++ WalletTransaction.Logic/TransactionRequestProcessor.cs
@@ -0,0 +1,25 @@
+﻿namespace WalletTransaction.Logic;
+
+public sealed class TransactionRequestProcessor : ITransactionsRequestProcessor
+{
+    private static async Task ProcessTransactionAsync(Transaction transaction, CancellationToken token)
+    {
+        token.ThrowIfCancellationRequested();
+
+        await Task.Delay(TimeSpan.FromMinutes(2), token);
+
+        transaction.Complete();
+    }
+
+    /// <inheritdoc/>
+    public async Task ProcessAsync(ITransactionsRequest transactionRequest, CancellationToken token)
+    {
+        ArgumentNullException.ThrowIfNull(transactionRequest);
+
+        await Task.WhenAll(transactionRequest.Transactions
+            .Select(x => ProcessTransactionAsync(x, token)));
+    }
+
+    /// <inheritdoc/>
+    public Task RefundAsync(ITransactionsRequest transactionRequest, CancellationToken token) => throw new NotImplementedException();
+}
