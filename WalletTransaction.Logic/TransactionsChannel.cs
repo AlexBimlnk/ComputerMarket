1058e24b5ab5d8a6df30708f60030ade953b1097
centuriin <centuriin@yandex.ru>
2022-11-07 21:01:54 +0300

    #14 add transaction channel

---
+++ WalletTransaction.Logic/TransactionsChannel.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Channels;
+
+namespace WalletTransaction.Logic;
+public sealed class TransactionsChannel
+{
+    private readonly Channel<(ITransactionsRequest, CancellationToken)> _channel = Channel.CreateUnbounded<(ITransactionsRequest, CancellationToken)>();
+
+    public ChannelReader<(ITransactionsRequest, CancellationToken)> ChannelReader => _channel.Reader;
+
+    public ChannelWriter<(ITransactionsRequest, CancellationToken)> ChannelWriter => _channel.Writer;
+}
