a9bf8a5b6466805bbbd497cf66caf2531a106590
w1sp <semen2002bs@gmail.com>
2022-11-15 17:05:12 +0300

    #19 add delete link command

---
+++ Market.Logic/Commands/Import/DeleteLinkCommand.cs
@@ -0,0 +1,44 @@
+﻿using Market.Logic.Models;
+
+namespace Market.Logic.Commands.Import;
+
+/// <summary xml:lang = "ru">
+/// Комманда на удаление связи.
+/// </summary>
+public sealed class DeleteLinkCommand : ImportCommand
+{
+    /// <summary xml:lang = "ru">
+    /// Создает новый экземпляр типа <see cref="DeleteLinkCommand"/>.
+    /// </summary>
+    /// <param name="id" xml:lang = "ru">
+    /// Идентификатор команды.
+    /// </param>
+    /// <param name="externalItemId" xml:lang = "ru">
+    /// Идентификатор продукта поставщика.
+    /// </param>
+    /// <param name="provider" xml:lang = "ru">
+    /// Поставщик.
+    /// </param>
+    /// <exception cref="ArgumentNullException" xml:lang = "ru">
+    /// Если <paramref name="provider"/> оказался <see langword="null"/>.
+    /// </exception>
+    public DeleteLinkCommand(
+        CommandId id,
+        InternalID externalItemId,
+        Provider provider)
+        : base(id, CommandType.DeleteLink)
+    {
+        ExternalItemId = externalItemId;
+        Provider = provider ?? throw new ArgumentNullException(nameof(provider));
+    }
+
+    /// <summary xml:lang = "ru">
+    /// Идентификатор продукта у постащика.
+    /// </summary>
+    public InternalID ExternalItemId { get; }
+
+    /// <summary xml:lang = "ru">
+    /// Поставщик.
+    /// </summary>
+    public Provider Provider { get; }
+}
---
+++ Market.Logic/Transport/Models/Commands/Import/DeleteLinkCommand.cs
@@ -0,0 +1,42 @@
+﻿using Market.Logic.Commands;
+
+using Newtonsoft.Json;
+
+namespace Market.Logic.Transport.Models.Commands.Import;
+
+/// <summary xml:lang = "ru">
+/// Транспортная модель команды для удаления связи.
+/// </summary>
+public sealed class DeleteLinkCommand : CommandBase
+{
+    /// <summary xml:lang = "ru">
+    /// Создает новый экземпляр типа <see cref="DeleteLinkCommand"/>.
+    /// </summary>
+    /// <param name="type" xml:lang = "ru">
+    /// Тип команды.
+    /// </param>
+    public DeleteLinkCommand(string id, CommandType type) : base(id, type) { }
+
+    /// <summary xml:lang = "ru">
+    /// Внешний идентификатор.
+    /// </summary>
+    [JsonProperty("external_id")]
+    public long ExternalID { get; set; }
+
+    /// <summary xml:lang = "ru">
+    /// Идентификатор провайдера.
+    /// </summary>
+    [JsonProperty("provider_id")]
+    public long ProviderID { get; set; } = default!;
+
+    public static DeleteLinkCommand ToModel(Logic.Commands.Import.DeleteLinkCommand command)
+    {
+        ArgumentNullException.ThrowIfNull(command);
+
+        return new DeleteLinkCommand(command.Id.Value, command.Type)
+        {
+            ExternalID = command.ExternalItemId.Value,
+            ProviderID = command.Provider.Key.Value
+        };
+    }
+}
