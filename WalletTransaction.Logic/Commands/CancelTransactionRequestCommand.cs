6bd8492fa93e201668be6a807cc3c31aa989fe5b
centuriin <centuriin@yandex.ru>
2022-11-15 19:58:03 +0300

    #101 add cancel command

---
+++ WalletTransaction.Logic/Commands/CancelTransactionRequestCommand.cs
@@ -0,0 +1,50 @@
+﻿using General.Logic.Commands;
+
+namespace WalletTransaction.Logic.Commands;
+
+/// <summary xml:lang = "ru">
+/// Команда отмены запроса на проведение транзакций.
+/// </summary>
+public sealed class CancelTransactionRequestCommand : CommandBase, ICommand
+{
+    private readonly ITransactionRequestCache _requestCache;
+    private readonly CancelTransactionRequestCommandParameters _parameters;
+
+    /// <summary xml:lang = "ru">
+    /// Создаёт новый экземпляр типа <see cref="CancelTransactionRequestCommand"/>.
+    /// </summary>
+    /// <param name="parameters" xml:lang = "ru">
+    /// Параметры команды.
+    /// </param>
+    /// <param name="requestCache" xml:lang = "ru">
+    /// Кэш запросов.
+    /// </param>
+    /// <exception cref="ArgumentNullException" xml:lang = "ru">
+    /// Если любой из входных параметров оказался <see langword="null"/>.
+    /// </exception>
+    public CancelTransactionRequestCommand(
+        CancelTransactionRequestCommandParameters parameters,
+        ITransactionRequestCache requestCache)
+    {
+        _parameters = parameters ?? throw new ArgumentNullException(nameof(parameters));
+        _requestCache = requestCache ?? throw new ArgumentNullException(nameof(requestCache));
+    }
+
+    /// <inheritdoc/>
+    public override CommandID Id => _parameters.Id;
+
+    protected override Task ExecuteCoreAsync()
+    {
+        var request = _requestCache.GetByKey(_parameters.TransactionRequestId);
+
+        if (request is null)
+            throw new InvalidOperationException(
+                $"Transaction request with id {_parameters.TransactionRequestId} is not exists.");
+
+        request.IsCancelled = true;
+
+        _requestCache.Delete(request);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ WalletTransaction.Logic/Commands/CancelTransactionRequestCommandParameters.cs
@@ -0,0 +1,27 @@
+﻿using General.Logic.Commands;
+
+namespace WalletTransaction.Logic.Commands;
+
+/// <summary xml:lang = "ru">
+/// Параметры команды для отмены запроса на проведение транзакций.
+/// </summary>
+public sealed class CancelTransactionRequestCommandParameters : CommandParametersBase
+{
+    /// <summary xml:lang = "ru"> 
+    /// Создает новый экземпляр типа <see cref="CancelTransactionRequestCommandParameters"/>.
+    /// </summary>
+    /// <param name="id"> 
+    /// Идентификатор запроса который необходимо отменить. 
+    /// </param>
+    public CancelTransactionRequestCommandParameters(
+        CommandID id,
+        InternalID requestId) : base(id)
+    {
+        TransactionRequestId = requestId;
+    }
+
+    /// <summary xml:lang = "ru">
+    /// Идентификатор запроса на проведение транзакций.
+    /// </summary>
+    public InternalID TransactionRequestId { get; }
+}
