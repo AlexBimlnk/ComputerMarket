21295ec4723dcae05275cd3661adb5fdbbf0ec7e
w1sp <semen2002bs@gmail.com>
2022-11-18 10:38:37 +0300

    #69 add command result

---
+++ Market.Logic/Commands/CommandResult.cs
@@ -0,0 +1,25 @@
+﻿using General.Logic.Commands;
+
+namespace Market.Logic.Commands;
+
+/// <summary xml:lang = "ru">
+/// Результат выполнения команды.
+/// </summary>
+public sealed class CommandResult : ICommandResult
+{
+    public CommandResult(CommandID id, string? errorMessge)
+    {
+        Id = id ?? throw new ArgumentNullException(nameof(id));
+
+        ErrorMessage = errorMessge;
+    }
+
+    /// <inheritdoc/>
+    public CommandID Id { get; }
+
+    /// <inheritdoc/>
+    public bool IsSuccess => ErrorMessage is null;
+
+    /// <inheritdoc/>
+    public string? ErrorMessage { get; }
+}
