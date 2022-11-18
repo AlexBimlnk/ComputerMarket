c20fd583d98f1cbc0ff8edc0ae53a9783ce473a4
centuriin <centuriin@yandex.ru>
2022-11-18 20:06:22 +0300

    #122 split on executer and processor

--- WalletTransaction.Logic/TransactionRequestExecuter.cs
+++ WalletTransaction.Logic/TransactionRequestExecuter.cs
@@ -1,9 +1,9 @@
 ﻿namespace WalletTransaction.Logic;
 
 /// <summary xml:lang = "ru">
-/// Обработчик запросов на проведение транзакций.
+/// Выполнитель запросов на проведение транзакций.
 /// </summary>
-public sealed class TransactionRequestProcessor : ITransactionsRequestProcessor
+public sealed class TransactionRequestExecuter : ITransactionRequestExecuter
 {
     private static async Task ProcessTransactionAsync(Transaction transaction, CancellationToken token)
     {
@@ -11,24 +11,29 @@ public sealed class TransactionRequestProcessor : ITransactionsRequestProcessor
 
         await Task.Delay(TimeSpan.FromMinutes(2), token);
 
+        // Симулирование ошибки транзакции (транзакция не выполнена)
+        if (Random.Shared.NextInt64() % 5 == 0)
+            return;
+
         transaction.Complete();
     }
 
     /// <inheritdoc/>
-    public async Task ProcessAsync(ITransactionsRequest transactionRequest, CancellationToken token)
+    public async Task ExecuteAsync(ITransactionsRequest transactionRequest, CancellationToken token)
     {
         ArgumentNullException.ThrowIfNull(transactionRequest);
 
         await Task.WhenAll(transactionRequest.Transactions
             .Select(x => ProcessTransactionAsync(x, token)));
+
+        var isSuccessful = transactionRequest.Transactions
+            .All(x => x.IsCompleted);
+
+        if (isSuccessful)
+            transactionRequest.CurrentState = TransactionRequestState.Finished;
+        else
+            transactionRequest.CurrentState = TransactionRequestState.Aborted;
     }
-
-    /// <inheritdoc/>
-    public Task ProcessAsync(CancellationToken token = default)
-    {
-
-    }
-
     /// <inheritdoc/>
     public async Task RefundAsync(ITransactionsRequest transactionRequest, CancellationToken token)
     {
@@ -38,5 +43,7 @@ public sealed class TransactionRequestProcessor : ITransactionsRequestProcessor
 
         await Task.WhenAll(transactionRequest.Transactions
             .Select(x => ProcessTransactionAsync(x, token)));
+
+        transactionRequest.CurrentState = TransactionRequestState.Refunded;
     }
 }
