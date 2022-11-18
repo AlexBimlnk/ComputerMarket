eeef08b10aae1ae39507a0203b28eaca90c24560
centuriin <centuriin@yandex.ru>
2022-11-18 20:48:40 +0300

    #122 add transaction processor

---
+++ WalletTransaction.Logic.Tests/TransactionRequestProcessor.Tests.cs
@@ -0,0 +1,165 @@
+﻿using General.Transport;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using WalletTransaction.Logic.Transport.Configurations;
+
+namespace WalletTransaction.Logic.Tests;
+public class TransactionRequestProcessorTests
+{
+    [Fact(DisplayName = $"The instance can create.")]
+    [Trait("Category", "Unit")]
+    public void CanBeCreated()
+    {
+        // Arrange
+        var logger = Mock.Of<ILogger<TransactionRequestProcessor>>(MockBehavior.Strict);
+        var receiver = Mock.Of<IReceiver<ITransactionsRequest>>(MockBehavior.Strict);
+        var resultSender = Mock.Of<ISender<TransactionsResultSenderConfiguration, ITransactionsRequest>>(MockBehavior.Strict);
+        var executer = Mock.Of<ITransactionRequestExecuter>(MockBehavior.Strict);
+
+        // Act
+        var exception = Record.Exception(() => _ = new TransactionRequestProcessor(
+            logger,
+            receiver,
+            resultSender,
+            executer));
+
+        // Assert
+        exception.Should().BeNull();
+    }
+
+    [Fact(DisplayName = $"The instance can't create without logger.")]
+    [Trait("Category", "Unit")]
+    public void CanNotBeCreatedWithoutLogger()
+    {
+        // Arrange
+        var receiver = Mock.Of<IReceiver<ITransactionsRequest>>(MockBehavior.Strict);
+        var resultSender = Mock.Of<ISender<TransactionsResultSenderConfiguration, ITransactionsRequest>>(MockBehavior.Strict);
+        var executer = Mock.Of<ITransactionRequestExecuter>(MockBehavior.Strict);
+
+        // Act
+        var exception = Record.Exception(() => _ = new TransactionRequestProcessor(
+            logger: null!,
+            receiver,
+            resultSender,
+            executer));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The instance can't create without receiver.")]
+    [Trait("Category", "Unit")]
+    public void CanNotBeCreatedWithoutReceiver()
+    {
+        // Arrange
+        var logger = Mock.Of<ILogger<TransactionRequestProcessor>>(MockBehavior.Strict);
+        var resultSender = Mock.Of<ISender<TransactionsResultSenderConfiguration, ITransactionsRequest>>(MockBehavior.Strict);
+        var executer = Mock.Of<ITransactionRequestExecuter>(MockBehavior.Strict);
+
+        // Act
+        var exception = Record.Exception(() => _ = new TransactionRequestProcessor(
+            logger,
+            receiver: null!,
+            resultSender,
+            executer));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The instance can't create without result sender.")]
+    [Trait("Category", "Unit")]
+    public void CanNotBeCreatedWithoutResultSender()
+    {
+        // Arrange
+        var logger = Mock.Of<ILogger<TransactionRequestProcessor>>(MockBehavior.Strict);
+        var receiver = Mock.Of<IReceiver<ITransactionsRequest>>(MockBehavior.Strict);
+        var executer = Mock.Of<ITransactionRequestExecuter>(MockBehavior.Strict);
+
+        // Act
+        var exception = Record.Exception(() => _ = new TransactionRequestProcessor(
+            logger,
+            receiver,
+            resultSender: null!,
+            executer));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The instance can't create without transactions executer.")]
+    [Trait("Category", "Unit")]
+    public void CanNotBeCreatedWithoutExecuter()
+    {
+        // Arrange
+        var logger = Mock.Of<ILogger<TransactionRequestProcessor>>(MockBehavior.Strict);
+        var receiver = Mock.Of<IReceiver<ITransactionsRequest>>(MockBehavior.Strict);
+        var resultSender = Mock.Of<ISender<TransactionsResultSenderConfiguration, ITransactionsRequest>>(MockBehavior.Strict);
+
+        // Act
+        var exception = Record.Exception(() => _ = new TransactionRequestProcessor(
+            logger,
+            receiver,
+            resultSender,
+            transactionRequestExecuter: null!));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The instance can process and stop.")]
+    [Trait("Category", "Unit")]
+    public async Task CanProcessAndStopAsync()
+    {
+        // Arrange
+        var logger = Mock.Of<ILogger<TransactionRequestProcessor>>();
+        var receiver = new Mock<IReceiver<ITransactionsRequest>>(MockBehavior.Strict);
+        var resultSender = new Mock<ISender<TransactionsResultSenderConfiguration, ITransactionsRequest>>(MockBehavior.Strict);
+        var executer = new Mock<ITransactionRequestExecuter>(MockBehavior.Strict);
+
+        var request = new Mock<ITransactionsRequest>(MockBehavior.Strict);
+        request.SetupGet(x => x.CurrentState)
+            .Returns(TransactionRequestState.WaitHandle);
+
+        var receiverCallbackCount = 0;
+        receiver.Setup(x => x.ReceiveAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult(request.Object))
+            .Callback(() => receiverCallbackCount++);
+
+        var executerCallbackCount = 0;
+        executer.Setup(x => x.ExecuteAsync(
+                request.Object,
+                It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask)
+            .Callback(() => executerCallbackCount++);
+
+        var resultSenderCallbackCount = 0;
+        resultSender.Setup(x => x.SendAsync(
+                request.Object,
+                It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask)
+            .Callback(() => resultSenderCallbackCount++);
+
+        using var cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(500));
+
+        var processor = new TransactionRequestProcessor(
+            logger,
+            receiver.Object,
+            resultSender.Object,
+            executer.Object);
+
+        // Act
+        var exception = await Record.ExceptionAsync(async () =>
+            await processor.ProcessAsync(cts.Token));
+
+        // Assert
+        exception.Should().NotBeNull()
+            .And.BeOfType<OperationCanceledException>();
+        receiverCallbackCount.Should().BeGreaterThan(0);
+        executerCallbackCount.Should().BeGreaterThan(0);
+        resultSenderCallbackCount.Should().BeGreaterThan(0);
+    }
+}
--- WalletTransaction.Logic/TransactionRequestProcessor.cs
+++ WalletTransaction.Logic/TransactionRequestProcessor.cs
@@ -1,42 +1,81 @@
-﻿namespace WalletTransaction.Logic;
+﻿using General.Transport;
+
+using Microsoft.Extensions.Logging;
+
+using WalletTransaction.Logic.Transport.Configurations;
+
+namespace WalletTransaction.Logic;
 
 /// <summary xml:lang = "ru">
 /// Обработчик запросов на проведение транзакций.
 /// </summary>
 public sealed class TransactionRequestProcessor : ITransactionsRequestProcessor
 {
-    private static async Task ProcessTransactionAsync(Transaction transaction, CancellationToken token)
+    private readonly ILogger<TransactionRequestProcessor> _logger;
+    private readonly IReceiver<ITransactionsRequest> _receiver;
+    private readonly ISender<TransactionsResultSenderConfiguration, ITransactionsRequest> _resultSender;
+    private readonly ITransactionRequestExecuter _transactionRequestExecuter;
+
+    /// <summary  xml:lang = "ru">
+    /// <see cref="TransactionRequestProcessor"/>
+    /// </summary>
+    /// <param name="logger"  xml:lang = "ru">
+    /// Логгер.
+    /// </param>
+    /// <param name="receiver"  xml:lang = "ru">
+    /// Получатель запросов на проведение транзакций.
+    /// </param>
+    /// <param name="resultSender"  xml:lang = "ru">
+    /// Отправитель результатов о проведении транзакции.
+    /// </param>
+    /// <param name="transactionRequestExecuter">
+    /// Исполнитель запросов на проведение транзакций.
+    /// </param>
+    /// <exception cref="ArgumentNullException" xml:lang = "ru">
+    /// Если любой из параметров оказался <see langword="null"/>.
+    /// </exception>
+    public TransactionRequestProcessor(
+        ILogger<TransactionRequestProcessor> logger, 
+        IReceiver<ITransactionsRequest> receiver, 
+        ISender<TransactionsResultSenderConfiguration, ITransactionsRequest> resultSender, 
+        ITransactionRequestExecuter transactionRequestExecuter)
     {
-        token.ThrowIfCancellationRequested();
-
-        await Task.Delay(TimeSpan.FromMinutes(2), token);
-
-        transaction.Complete();
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _receiver = receiver ?? throw new ArgumentNullException(nameof(receiver));
+        _resultSender = resultSender ?? throw new ArgumentNullException(nameof(resultSender));
+        _transactionRequestExecuter = transactionRequestExecuter ?? throw new ArgumentNullException(nameof(transactionRequestExecuter));
     }
 
     /// <inheritdoc/>
-    public async Task ProcessAsync(ITransactionsRequest transactionRequest, CancellationToken token)
+    public async Task ProcessAsync(CancellationToken token = default)
     {
-        ArgumentNullException.ThrowIfNull(transactionRequest);
+        _logger.LogInformation("Start receive and process requests..");
 
-        await Task.WhenAll(transactionRequest.Transactions
-            .Select(x => ProcessTransactionAsync(x, token)));
-    }
+        while (true)
+        {
+            token.ThrowIfCancellationRequested();
 
-    /// <inheritdoc/>
-    public Task ProcessAsync(CancellationToken token = default)
-    {
+            var request = await _receiver.ReceiveAsync(token);
 
-    }
+            _logger.LogDebug("Received new request");
 
-    /// <inheritdoc/>
-    public async Task RefundAsync(ITransactionsRequest transactionRequest, CancellationToken token)
-    {
-        ArgumentNullException.ThrowIfNull(transactionRequest);
+            if (request.CurrentState is TransactionRequestState.Finished)
+                _logger.LogWarning("Request already finished! Skip");
 
-        // some other logic
+            if (request.CurrentState is TransactionRequestState.Aborted)
+                _logger.LogWarning("Request has aborted status");
 
-        await Task.WhenAll(transactionRequest.Transactions
-            .Select(x => ProcessTransactionAsync(x, token)));
+            await _transactionRequestExecuter.ExecuteAsync(request, token);
+
+            _logger.LogDebug(
+                "Processing comlete with request status: {Status}",
+                nameof(request.CurrentState));
+
+            await _resultSender.SendAsync(request, token);
+
+            _logger.LogDebug("Sending result...");
+
+            _logger.LogDebug("Request has been processed");
+        }
     }
 }
