6cd9a91a2f795d55b29d011ca5961cf1b0d0a6e7
centuriin <centuriin@yandex.ru>
2022-11-20 13:26:38 +0300

    #124 add deserialzier

---
+++ WalletTransaction.Logic/Transport/Deserializers/CommandParametersDeserializer.cs
@@ -0,0 +1,63 @@
+﻿using General.Logic.Commands;
+using General.Transport;
+
+using Newtonsoft.Json;
+
+using WalletTransaction.Logic.Commands;
+using WalletTransaction.Logic.Transport.Models.Commands;
+
+namespace WalletTransaction.Logic.Transport.Deserializers;
+
+using TCommand = Models.Commands.CommandBase;
+
+/// <summary xml:lang = "ru">
+/// Дессериализатор из <see cref="string"/> в <see cref="CommandParametersBase"/>.
+/// </summary>
+public sealed class CommandParametersDeserializer : IDeserializer<string, CommandParametersBase>
+{
+    private readonly JsonSerializer _serializer = JsonSerializer.CreateDefault(new JsonSerializerSettings
+    {
+        Converters = new[]
+        {
+            new CommandConverter()
+        }
+    });
+
+    /// <inheritdoc/>
+    /// <exception cref="ArgumentException" xml:lang = "ru">
+    /// Когда <paramref name="source"/> имел неверный формат.
+    /// </exception>
+    public CommandParametersBase Deserialize(string source)
+    {
+        if (string.IsNullOrWhiteSpace(source))
+            throw new ArgumentException(
+                "The source is null, has only whitespaces or empty.",
+                nameof(source));
+
+        using var reader = new JsonTextReader(new StringReader(source));
+
+        TCommand trasnportCommand = _serializer.Deserialize<TCommand>(reader)!;
+
+        return trasnportCommand switch
+        {
+            CreateRequestCommand createRequestCommand => new CreateTransactionRequestCommandParameters(
+                new(createRequestCommand.Id),
+                new TransactionRequest(
+                    new(createRequestCommand.TransactionRequest.Id),
+                    createRequestCommand.TransactionRequest.Transactions
+                        .Select(x => Models.Transaction.FromModel(x))
+                        .ToList())),
+
+            CancelRequestCommand cancelCommand => new CancelTransactionRequestCommandParameters(
+                new(cancelCommand.Id),
+                new(cancelCommand.RequestID)),
+
+            FinishRequestCommand finishCommand => new FinishTransactionRequestCommandParameters(
+                new(finishCommand.Id),
+                new(finishCommand.RequestID)),
+
+            var unknownCommandType =>
+                throw new InvalidOperationException($"The source contains unknown command '{unknownCommandType?.GetType().Name}'. ")
+        };
+    }
+}
