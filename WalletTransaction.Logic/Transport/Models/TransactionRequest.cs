ff2d3704980c655830034aa31d2722a3fc5267b2
centuriin <centuriin@yandex.ru>
2022-11-20 13:16:39 +0300

    #124 add missed json prop

--- WalletTransaction.Logic/Transport/Models/TransactionRequest.cs
+++ WalletTransaction.Logic/Transport/Models/TransactionRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace WalletTransaction.Logic.Transport.Models;
+﻿using Newtonsoft.Json;
+
+namespace WalletTransaction.Logic.Transport.Models;
 
 /// <summary xml:lang = "ru">
 /// Транспортная модель запроса на проведение транзаций.
@@ -8,10 +10,12 @@ public sealed class TransactionRequest
     /// <summary xml:lang = "ru">
     /// Идентификатор запроса.
     /// </summary>
+    [JsonProperty("id", Required = Required.Always)]
     public long Id { get; set; }
 
     /// <summary xml:lang = "ru">
     /// Коллекция транзакций, которые нужно выполнить в рамках запроса.
     /// </summary>
+    [JsonProperty("transactions", Required = Required.Always)]
     public IReadOnlyCollection<Transaction> Transactions { get; set; } = default!;
 }
