e28535d26b727f938df020937b4d96e56c43f3f0
centuriin <centuriin@yandex.ru>
2022-11-20 14:18:33 +0300

    #124 fix doc

--- WalletTransaction.Logic/Transport/Models/Commands/CommandType.cs
+++ WalletTransaction.Logic/Transport/Models/Commands/CommandType.cs
@@ -5,7 +5,7 @@ using Newtonsoft.Json.Serialization;
 namespace WalletTransaction.Logic.Transport.Models.Commands;
 
 /// <summary xml:lang = "ru">
-/// Типы команд обрабатываемых сервисом импорта.
+/// Типы команд обрабатываемых сервисом банковский транзакций.
 /// </summary>
 [JsonConverter(typeof(StringEnumConverter), typeof(SnakeCaseNamingStrategy))]
 public enum CommandType
