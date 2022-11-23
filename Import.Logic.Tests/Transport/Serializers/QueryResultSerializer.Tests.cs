fb5775294bc0d0f6695ef6f40272212b45ef8abf
w1sp <semen2002bs@gmail.com>
2022-11-23 23:24:35 +0300

    #92 add query result serializer

---
+++ Import.Logic.Tests/Transport/Serializers/QueryResultSerializer.Tests.cs
@@ -0,0 +1,76 @@
+﻿using General.Logic.Executables;
+using General.Logic.Queries;
+
+using Import.Logic.Models;
+using Import.Logic.Queries;
+using Import.Logic.Transport.Serializers;
+
+namespace Import.Logic.Tests.Transport.Serializers;
+public class QueryResultSerializerTests
+{
+    [Fact(DisplayName = $"The instance can create.")]
+    [Trait("Category", "Unit")]
+    public void CanBeCreated()
+    {
+        // Act
+        var exception = Record.Exception(() =>
+            _ = new QueryResultSerializer());
+
+        // Assert
+        exception.Should().BeNull();
+    }
+
+    [Theory(DisplayName = $"The instance can serialize.")]
+    [Trait("Category", "Unit")]
+    [MemberData(nameof(SerializeData))]
+    public void CanSerialize(IQueryResult products, string expectedResult)
+    {
+        // Arrange
+        var serializer = new QueryResultSerializer();
+
+        string actual = null!;
+
+        // Act
+        var exception = Record.Exception(() =>
+            actual = serializer.Serialize(products));
+
+        // Assert
+        exception.Should().BeNull();
+        actual.Should().BeEquivalentTo(expectedResult);
+    }
+
+    [Fact(DisplayName = $"The instance can't serialize null.")]
+    [Trait("Category", "Unit")]
+    public void CanNotSerializeNull()
+    {
+        // Arrange
+        var serializer = new QueryResultSerializer();
+
+        // Act
+        var exception = Record.Exception(() =>
+            _ = serializer.Serialize(null!));
+
+        // Assert
+        exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    public readonly static TheoryData<IQueryResult, string> SerializeData = new()
+    {
+        {
+            QueryResult<FakeResultObject>.Success(new ExecutableID("123"), new FakeResultObject()
+            {
+                Value = 1
+            }),
+            /*lang=json,strict*/@"{""id"":""123"",""error_message"":null,""result"":{""Value"":1}}"
+        },
+        {
+            QueryResult<IReadOnlyCollection<Product>>.Fail(new ExecutableID("123"), "some error name"),
+            /*lang=json,strict*/@"{""id"":""123"",""error_message"":""some error name"",""result"":null}"
+        }
+    };
+
+    private sealed class FakeResultObject
+    {
+        public int Value { get; set; }
+    }
+}
---
+++ Import.Logic/Transport/Serializers/QueryResultSerializer.cs
@@ -0,0 +1,39 @@
+﻿using General.Logic.Queries;
+using General.Transport;
+
+using Newtonsoft.Json;
+
+namespace Import.Logic.Transport.Serializers;
+
+/// <summary xml:lang = "ru">
+/// Сериализатор запросов.
+/// </summary>
+public class QueryResultSerializer : ISerializer<IQueryResult, string>
+{
+    /// <inheritdoc/>
+    public string Serialize(IQueryResult source)
+    {
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+
+        var transportResult = new TQueryResult()
+        {
+            Id = source.Id.Value,
+            ErrorMessage = source.ErrorMessage,
+            Result = source.Result
+        };
+
+        return JsonConvert.SerializeObject(transportResult);
+    }
+
+    private sealed class TQueryResult
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("error_message")]
+        public string? ErrorMessage { get; set; }
+
+        [JsonProperty("result")]
+        public object? Result { get; set; }
+    }
+}
