a102fbb28681f880bc13eb3fc3574f2481eecb09
centuriin <centuriin@yandex.ru>
2022-11-27 14:14:59 +0300

    #140 create view 'create new'

---
+++ Market/Models/LinkViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Market.Models;
+
+public sealed class LinkViewModel
+{
+    [Required(ErrorMessage = "Не указан внутренний идентификатор продукта.")]
+    [Range(1, long.MaxValue, ErrorMessage = "Идентификатор не может быть отрицательным.")]
+    public long InternalId { get; set; }
+
+    [Required(ErrorMessage = "Не указан внутренний идентификатор продукта.")]
+    [Range(1, long.MaxValue, ErrorMessage = "Идентификатор не может быть отрицательным.")]
+    public long ProviderId { get; set; }
+
+    [Required(ErrorMessage = "Не указан внутренний идентификатор продукта.")]
+    [Range(1, long.MaxValue, ErrorMessage = "Идентификатор не может быть отрицательным.")]
+    public long ExternalId { get; set; }
+}
