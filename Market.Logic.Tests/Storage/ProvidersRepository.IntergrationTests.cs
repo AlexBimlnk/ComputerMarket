e89a1586b5dc14520dadf1347f8d95949a3ad2e8
w1sp <semen2002bs@gmail.com>
2022-11-28 11:11:44 +0300

    #110 add tests

---
+++ Market.Logic.Tests/Storage/ProvidersRepository.IntergrationTests.cs
@@ -0,0 +1,204 @@
+﻿using Market.Logic.Models;
+using Market.Logic.Storage.Repositories;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+namespace Market.Logic.Tests.Storage;
+
+using TProvider = Logic.Storage.Models.Provider;
+
+public class ProvidersRepositoryIntegrationTests : DBIntegrationTestBase
+{
+    public ProvidersRepositoryIntegrationTests()
+        : base(nameof(ProvidersRepositoryIntegrationTests)) { }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} can add product.")]
+    [Trait("Category", "Integration")]
+    public async Task CanAddProviderAsync()
+    {
+        // Arrange
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        context.SetupGet(x => x.Providers)
+            .Returns(_marketContext.Providers);
+
+        var inputProvider = new Provider(
+            new ID(1),
+            "Provider Name 1",
+            new Margin(1.3m),
+            new PaymentTransactionsInformation(
+                inn: "1234512345",
+                bankAccount: "12345123451234512345"));
+
+        var expectedProvider = new TProvider[]
+        {
+            new TProvider
+            {
+                Id = 1,
+                Name = "Provider Name 1",
+                Margin = 1.3m,
+                Inn = "1234512345",
+                BankAccount = "12345123451234512345"
+            }
+        };
+
+        var repository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        // Act
+        using var transaction = _marketContext.Database
+            .BeginTransaction();
+
+        await repository.AddAsync(inputProvider)
+            .ConfigureAwait(false);
+
+        await _marketContext.SaveChangesAsync()
+            .ConfigureAwait(false);
+
+        await transaction.CommitAsync()
+            .ConfigureAwait(false);
+
+        var dbProviders = await GetTableRecordsAsync(
+            "providers",
+            r => new TProvider
+            {
+                Id = r.GetInt64(0),
+                Name = r.GetString(1),
+                Margin = r.GetDecimal(2),
+                BankAccount = r.GetString(3),
+                Inn = r.GetString(4)
+            });
+
+        // Assert
+        dbProviders.Should().BeEquivalentTo(expectedProvider);
+    }
+
+    [Theory(DisplayName = $"The {nameof(ProvidersRepository)} can contains works.")]
+    [Trait("Category", "Integration")]
+    [MemberData(nameof(ContainsData))]
+    public async Task CanContainsWorksAsync(Provider inputProvider, bool expectedResult)
+    {
+        // Arrange
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        context.SetupGet(x => x.Providers)
+            .Returns(_marketContext.Providers);
+        var provider = new Provider(
+            new ID(1),
+            "Provider Name 1",
+            new Margin(1.3m),
+            new PaymentTransactionsInformation(
+                inn: "1234512345",
+                bankAccount: "12345123451234512345"));
+
+        await AddProviderAsync(provider);
+        
+        var repository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        // Act
+        var result = await repository.ContainsAsync(inputProvider)
+            .ConfigureAwait(false);
+
+        // Assert
+        result.Should().Be(expectedResult);
+    }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} can delete product.")]
+    [Trait("Category", "Integration")]
+    public async Task CanDeleteProviderAsync()
+    {
+        // Arrange
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        context.SetupGet(x => x.Providers)
+            .Returns(_marketContext.Providers);
+        context.Setup(x => x.SaveChanges())
+            .Callback(() => _marketContext.SaveChanges());
+
+        var provider1 = new Provider(
+            id: new ID(1),
+            "name1",
+            new Margin(1.3m),
+            new PaymentTransactionsInformation(
+                inn: "1234512345",
+                bankAccount: "12345123451234512345"));
+
+        
+        await AddProviderAsync(provider1);
+        
+        var repository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        var inputProvider = new Provider(
+            new ID(1),
+            "Provider Name 1",
+            new Margin(1.3m),
+            new PaymentTransactionsInformation(
+                inn: "1234512345",
+                bankAccount: "12345123451234512345"));
+
+        // Act
+        var beforeContains = await repository.ContainsAsync(inputProvider)
+            .ConfigureAwait(false);
+
+        var exception = Record.Exception(() =>
+        {
+            repository.Delete(inputProvider);
+            repository.Save();
+        });
+
+        var afterContains = await repository.ContainsAsync(inputProvider)
+            .ConfigureAwait(false);
+
+        // Assert
+        beforeContains.Should().BeTrue();
+        exception.Should().BeNull();
+        afterContains.Should().BeFalse();
+    }
+
+    private async Task AddProviderAsync(Provider provider)
+    {
+        var fromQuery = "providers (id, name, margin, bank_account, inn)";
+        var valuesQuery =
+            $"({provider.Key.Value}, " +
+            $"'{provider.Name}', " +
+            $"{provider.Margin.Value.ToString(System.Globalization.CultureInfo.InvariantCulture)}, " +
+            $"'{provider.PaymentTransactionsInformation.BankAccount}', " +
+            $"'{provider.PaymentTransactionsInformation.INN}')";
+
+        await AddAsync(fromQuery, valuesQuery);
+    }
+
+    public static readonly TheoryData<Provider, bool> ContainsData = new()
+    {
+        {
+            new Provider(
+                new ID(1),
+                "Provider Name 1",
+                new Margin(1.3m),
+                new PaymentTransactionsInformation(
+                    inn: "1234512345",
+                    bankAccount: "12345123451234512345")),
+            true
+        },
+        {
+            new Provider(
+                new ID(2),
+                "Provider Name 1",
+                new Margin(1.3m),
+                new PaymentTransactionsInformation(
+                    inn: "1234512344",
+                    bankAccount: "12345123451234512344")),
+            false
+        },
+    };
+}
---
+++ Market.Logic.Tests/Storage/ProvidersRepository.Tests.cs
@@ -0,0 +1,446 @@
+﻿using Market.Logic.Models;
+using Market.Logic.Storage.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+namespace Market.Logic.Tests.Storage;
+
+using TProvider = Logic.Storage.Models.Provider;
+
+public class ProvidersRepositoryTests
+{
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} can be created.")]
+    [Trait("Category", "Unit")]
+    public void CanBeCreated()
+    {
+        // Act
+        var exception = Record.Exception(() => _ = new ProvidersRepository(
+            Mock.Of<IRepositoryContext>(MockBehavior.Strict),
+            Mock.Of<ILogger<ProvidersRepository>>(MockBehavior.Strict)));
+
+        // Assert
+        exception.Should().BeNull();
+    }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} cannot be created when repository context is null.")]
+    [Trait("Category", "Unit")]
+    public void CanNotCreateWithoutRepositoryContext()
+    {
+        // Act
+        var exception = Record.Exception(() => _ = new ProvidersRepository(
+            context: null!,
+            Mock.Of<ILogger<ProvidersRepository>>()));
+
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} cannot be created when logger is null.")]
+    [Trait("Category", "Unit")]
+    public void CanNotCreateWithoutLogger()
+    {
+        // Act
+        var exception = Record.Exception(() => _ = new ProvidersRepository(
+            Mock.Of<IRepositoryContext>(),
+            logger: null!));
+
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} can add product.")]
+    [Trait("Category", "Unit")]
+    public async void CanAddProductAsync()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var storageProvider = new TProvider()
+        {
+            Id = 1,
+            Name = "Provider Name 1",
+            Margin = 1.3m,
+            Inn = "1234512345", 
+            BankAccount = "12345123451234512345"
+        };
+
+        var providers = new Mock<DbSet<TProvider>>(MockBehavior.Strict);
+        var providersCallback = 0;
+        providers.Setup(x => x
+            .AddAsync(
+                It.Is<TProvider>(p =>
+                    p.Id == storageProvider.Id &&
+                    p.Name == storageProvider.Name &&
+                    p.Margin == storageProvider.Margin &&
+                    p.Inn == storageProvider.Inn &&
+                    p.BankAccount == storageProvider.BankAccount),
+                It.IsAny<CancellationToken>()))
+            .Callback(() => providersCallback++)
+            .Returns(new ValueTask<EntityEntry<TProvider>>());
+
+        context.Setup(x => x.Providers)
+            .Returns(providers.Object);
+
+        var providersRepository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        var inputProvider = new Provider(
+                id: new ID(1),
+                name: "Provider Name 1",
+                new Margin(1.3m),
+                new PaymentTransactionsInformation(
+                    "1234512345",
+                    "12345123451234512345"));
+
+        // Act
+        var exception = await Record.ExceptionAsync(async () =>
+            await providersRepository.AddAsync(inputProvider));
+
+        // Assert
+        exception.Should().BeNull();
+        providersCallback.Should().Be(1);
+    }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} cannot add product when product is null.")]
+    [Trait("Category", "Unit")]
+    public async void CanNotAddProductWhenProductIsNullAsync()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var providersRepository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        // Act
+        var exception = await Record.ExceptionAsync(async () =>
+            await providersRepository.AddAsync(null!));
+
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} can cancel add product.")]
+    [Trait("Category", "Unit")]
+    public async void CanCancelAddProductAsync()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var cts = new CancellationTokenSource();
+
+        var providerRepository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        var inputProvider = new Provider(
+                id: new ID(1),
+                name: "Provider Name 1",
+                new Margin(1.3m),
+                new PaymentTransactionsInformation(
+                    "1234512345",
+                    "12345123451234512345"));
+
+        // Act
+        cts.Cancel();
+        var exception = await Record.ExceptionAsync(async () =>
+            await providerRepository.AddAsync(inputProvider, cts.Token));
+
+        // Assert
+        exception.Should().BeOfType<OperationCanceledException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} cannot contains product when product is null.")]
+    [Trait("Category", "Unit")]
+    public async void CanNotContainsWhenProductIsNullAsync()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var providersRepository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        // Act
+        var exception = await Record.ExceptionAsync(async () =>
+            await providersRepository.ContainsAsync(null!));
+
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} can cancel contains product.")]
+    [Trait("Category", "Unit")]
+    public async void CanCancelContainsAsync()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var cts = new CancellationTokenSource();
+
+        var providersRepository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        var inputProvider = new Provider(
+                id: new ID(1),
+                name: "Provider Name 1",
+                new Margin(1.3m),
+                new PaymentTransactionsInformation(
+                    "1234512345",
+                    "12345123451234512345"));
+
+        // Act
+        cts.Cancel();
+        var exception = await Record.ExceptionAsync(async () =>
+            await providersRepository.ContainsAsync(inputProvider, cts.Token));
+
+        // Assert
+        exception.Should().BeOfType<OperationCanceledException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} can delete product.")]
+    [Trait("Category", "Unit")]
+    public void CanDeleteProduct()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var storageProvider = new TProvider()
+        {
+            Id = 1,
+            Name = "Provider Name 1",
+            Margin = 1.3m,
+            Inn = "1234512345",
+            BankAccount = "12345123451234512345"
+        };
+
+        var providers = new Mock<DbSet<TProvider>>(MockBehavior.Loose);
+
+        context.Setup(x => x.Providers)
+            .Returns(providers.Object);
+
+        var providersRepository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        var containsProvider = new Provider(
+                id: new ID(1),
+                name: "Provider Name 1",
+                new Margin(1.3m),
+                new PaymentTransactionsInformation(
+                    "1234512345",
+                    "12345123451234512345"));
+
+        // Act
+        var exception = Record.Exception(() =>
+            providersRepository.Delete(containsProvider));
+
+        // Assert
+        exception.Should().BeNull();
+
+        providers.Verify(x =>
+            x.Remove(
+                It.Is<TProvider>(p =>
+                    p.Id == storageProvider.Id &&
+                    p.Name == storageProvider.Name &&
+                    p.Margin == storageProvider.Margin&&
+                    p.Inn == storageProvider.Inn &&
+                    p.BankAccount == storageProvider.BankAccount)),
+            Times.Once);
+    }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} cannot delete product when product is null.")]
+    [Trait("Category", "Unit")]
+    public void CanNotDeleteWhenProductIsNull()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var providersRepository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        // Act
+        var exception = Record.Exception(() =>
+            providersRepository.Delete(null!));
+
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} can get product by key.")]
+    [Trait("Category", "Unit")]
+    public void CanGetByKey()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var data = new List<TProvider>
+        {
+            new TProvider()
+            {
+                Id = 1,
+                Name = "Provider Name 1",
+                Margin = 1.3m,
+                Inn = "1234512345",
+                BankAccount = "12345123451234512345"
+            }
+        }.AsQueryable();
+
+        var providers = new Mock<DbSet<TProvider>>(MockBehavior.Strict);
+
+        providers
+            .As<IQueryable<TProvider>>()
+            .Setup(m => m.Provider)
+            .Returns(data.Provider);
+        providers
+            .As<IQueryable<TProvider>>()
+            .Setup(m => m.Expression)
+            .Returns(data.Expression);
+        providers
+            .As<IQueryable<TProvider>>()
+            .Setup(m => m.ElementType)
+            .Returns(data.ElementType);
+        providers
+            .As<IQueryable<TProvider>>()
+            .Setup(m => m.GetEnumerator())
+            .Returns(() => data.GetEnumerator());
+
+        context.Setup(x => x.Providers)
+            .Returns(providers.Object);
+
+        var providersRepository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        var expectedResult = new Provider(
+                id: new ID(1),
+                name: "Provider Name 1",
+                new Margin(1.3m),
+                new PaymentTransactionsInformation(
+                    "1234512345",
+                    "12345123451234512345"));
+
+        // Act
+        var result1 = providersRepository.GetByKey(new ID(1));
+        var result2 = providersRepository.GetByKey(new ID(2));
+
+        // Assert
+        result1.Should().NotBeNull();
+        result1.Should().BeEquivalentTo(expectedResult);
+        result2.Should().BeNull();
+    }
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} can get all products.")]
+    [Trait("Category", "Unit")]
+    public void CanGetEntities()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Strict);
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var data = new List<TProvider>
+        {
+            new TProvider()
+            {
+                Id = 1,
+                Name = "Provider Name 1",
+                Margin = 1.3m,
+                Inn = "1234512345",
+                BankAccount = "12345123451234512345"
+            }
+        }.AsQueryable();
+
+        var providers = new Mock<DbSet<TProvider>>(MockBehavior.Strict);
+
+        providers
+            .As<IQueryable<TProvider>>()
+            .Setup(m => m.Provider)
+            .Returns(data.Provider);
+        providers
+            .As<IQueryable<TProvider>>()
+            .Setup(m => m.Expression)
+            .Returns(data.Expression);
+        providers
+            .As<IQueryable<TProvider>>()
+            .Setup(m => m.ElementType)
+            .Returns(data.ElementType);
+        providers
+            .As<IQueryable<TProvider>>()
+            .Setup(m => m.GetEnumerator())
+            .Returns(() => data.GetEnumerator());
+
+        context.Setup(x => x.Providers)
+            .Returns(providers.Object);
+
+        var productRepository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        var expectedResult = new List<Provider>()
+        {
+            new Provider(
+                id: new ID(1),
+                name: "Provider Name 1",
+                new Margin(1.3m),
+                new PaymentTransactionsInformation(
+                    "1234512345",
+                    "12345123451234512345"))
+        };
+
+        // Act
+        var result = productRepository.GetEntities();
+
+        // Assert
+        result.Should().BeEquivalentTo(expectedResult, opt => opt.WithStrictOrdering());
+    }
+
+
+    [Fact(DisplayName = $"The {nameof(ProvidersRepository)} can save.")]
+    [Trait("Category", "Unit")]
+    public void CanSave()
+    {
+        // Arrange
+        var context = new Mock<IRepositoryContext>(MockBehavior.Loose);
+        var logger = Mock.Of<ILogger<ProvidersRepository>>();
+
+        var storageProduct = new TProvider()
+        {
+            Id = 1,
+            Name = "Provider Name 1",
+            Margin = 1.3m,
+            Inn = "1234512345",
+            BankAccount = "12345123451234512345"
+        };
+
+        var providersRepository = new ProvidersRepository(
+            context.Object,
+            logger);
+
+        // Act
+        var exception = Record.Exception(() =>
+            providersRepository.Save());
+
+        // Assert
+        exception.Should().BeNull();
+
+        context.Verify(x =>
+            x.SaveChanges(),
+            Times.Once);
+    }
+}
+
