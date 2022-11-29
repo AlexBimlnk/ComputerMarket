541471e4c7b777993f35da1019ae2ae29b61270c
w1sp <semen2002bs@gmail.com>
2022-11-29 17:25:46 +0300

    #143 change db migration

---
+++ Databases/Market/Migrations/9_change_item_property.sql
@@ -0,0 +1,25 @@
+BEGIN;
+
+DELETE FROM db_version WHERE TRUE;
+
+INSERT INTO db_version
+VALUES (9);
+
+DROP TABLE IF EXISTS property_data_type;
+
+CREATE TABLE property_data_type(
+    Id SMALLSERIAL PRIMARY KEY,
+    Name VARCHAR(10));
+
+INSERT INTO property_data_type(Id, Name) VALUES (1, 'string'), (2, 'int');
+
+ALTER TABLE item_properties
+DROP COLUMN property_data_type;
+
+ALTER TABLE item_properties
+ADD COLUMN data_type_id INT NOT NULL DEFAULT 1;
+
+ALTER TABLE item_properties
+ADD CONSTRAINT property_data_type_id_fkey FOREIGN KEY (data_type_id) REFERENCES property_data_type(Id);
+
+COMMIT;
