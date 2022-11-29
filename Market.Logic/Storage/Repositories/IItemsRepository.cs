14832ee3ccf8bfdae143dd0e23b156e241674fff
w1sp <semen2002bs@gmail.com>
2022-11-29 17:27:58 +0300

    #143 add item repository

---
+++ Market.Logic/Storage/Repositories/IItemsRepository.cs
@@ -0,0 +1,9 @@
+﻿using General.Storage;
+
+using Market.Logic.Models;
+
+namespace Market.Logic.Storage.Repositories;
+public interface IItemsRepository : IKeyableRepository<Item, ID>
+{
+    public void AddOrUpdate(Item item);
+}
