8f9350918e882fd795ff184352183fb3127eeb8f
centuriin <centuriin@yandex.ru>
2022-12-04 13:32:56 +0300

    #107 fix tests

---
+++ WalletTransaction.Logic/Transport/Models/Commands/RefundRequestCommand.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace WalletTransaction.Logic.Transport.Models.Commands;
+
+/// <summary xml:lang = "ru">
+/// Транспортная модель команды возврата средств по запросу.
+/// </summary>
+public sealed class RefundRequestCommand : CommandBase
+{
+    /// <summary xml:lang = "ru">
+    /// Создает новый экземпляр типа <see cref="RefundRequestCommand"/>.
+    /// </summary>
+    /// <param name="type" xml:lang = "ru">
+    /// Тип команды.
+    /// </param>
+    public RefundRequestCommand(CommandType type, string id) : base(type, id) { }
+
+    /// <summary xml:lang = "ru">
+    /// Идентификатор запроса.
+    /// </summary>
+    [JsonProperty("request_id", Required = Required.Always)]
+    public long RequestID { get; set; }
+}
