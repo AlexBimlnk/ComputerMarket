b236e1e0d2092ecb87233775a47ef408d2586fe2
centuriin <centuriin@yandex.ru>
2022-12-06 21:31:53 +0300

    #158 add report and report builder

---
+++ Market.Logic.Tests/Reports/Report.Tests.cs
@@ -0,0 +1,62 @@
+﻿using Market.Logic.Reports;
+
+namespace Market.Logic.Tests.Models;
+
+public class ReportTests
+{
+    [Fact(DisplayName = $"The {nameof(Report)} can be created.")]
+    [Trait("Category", "Unit")]
+    public void CanBeCreated()
+    {
+        // Arrange
+        Report report = null!;
+        var provider = TestHelper.GetOrdinaryProvider();
+        var soldProductsCount = 12;
+        var productMVP = TestHelper.GetOrdinaryProduct();
+        var totalProfit = 123m;
+
+        // Act
+        var exception = Record.Exception(() => report = new Report(
+            provider,
+            soldProductsCount,
+            productMVP,
+            totalProfit));
+
+        // Assert
+        exception.Should().BeNull();
+        report.Provider.Should().Be(provider);
+        report.SoldProductsCount.Should().Be(soldProductsCount);
+        report.ProductMVP.Should().Be(productMVP);
+        report.TotalProfit.Should().Be(totalProfit);
+    }
+
+    [Fact(DisplayName = $"The {nameof(Report)} cannot be created without provider.")]
+    [Trait("Category", "Unit")]
+    public void CanNotCreateWithoutProvider()
+    {
+        // Act
+        var exception = Record.Exception(() => _ = new Report(
+            provider: null!,
+            soldProductsCount: 13,
+            productMVP: TestHelper.GetOrdinaryProduct(),
+            totalProfit: 123.3m));
+
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = $"The {nameof(Report)} cannot be created without product mvp.")]
+    [Trait("Category", "Unit")]
+    public void CanNotCreateWithoutProductMVP()
+    {
+        // Act
+        var exception = Record.Exception(() => _ = new Report(
+            TestHelper.GetOrdinaryProvider(),
+            soldProductsCount: 13,
+            productMVP: null!,
+            totalProfit: 123.3m));
+
+        // Assert
+        exception.Should().BeOfType<ArgumentNullException>();
+    }
+}
