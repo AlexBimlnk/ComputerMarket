04a3c84ab75025a33fa58cce81f8f3dcf398c445
w1sp <semen2002bs@gmail.com>
2022-12-07 11:43:21 +0300

    #146 add basket rep

--- Market.Logic/Storage/Repositories/IOrderRepository.cs
+++ Market.Logic/Storage/Repositories/IOrderRepository.cs
@@ -14,25 +14,4 @@ public interface IOrderRepository : IKeyableRepository<Order, ID>
     /// </summary>
     /// <param name="order" xml:lang="ru">Заказ с новым состоянием.</param>
     public void UpdateState(Order order);
-
-    /// <summary xml:lang="ru">
-    /// Добавление продукта в корзину.
-    /// </summary>
-    /// <param name="user" xml:lang="ru">Пользоватлеь корзины.</param>
-    /// <param name="entity" xml:lang="ru">Продут, который бдует добавлен.</param>
-    public void AddToBasket(User user, PurchasableEntity entity);
-
-    /// <summary xml:lang="ru">
-    /// Удаление из продукта корзины.
-    /// </summary>
-    /// <param name="user" xml:lang="ru">Пользователь корзины.</param>
-    /// <param name="entity" xml:lang="ru">Продукт который будет удален из корзины.</param>
-    public void RemoveFromBasket(User user, PurchasableEntity entity);
-
-    /// <summary xml:lang="ru">
-    /// Получение коллекции продуктво в корзине.
-    /// </summary>
-    /// <param name="user" xml:lang="ru">Пользователь корзины.</param>
-    /// <returns xml:lang="ru">Продукты в корзине пользователя.</returns>
-    public IEnumerable<PurchasableEntity> GetAllBasketItems(User user);
 }
