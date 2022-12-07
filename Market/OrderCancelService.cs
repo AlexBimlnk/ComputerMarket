6d31a45eb631b0f4a20d624be78534ff83f6cc27
centuriin <centuriin@yandex.ru>
2022-12-07 20:52:49 +0300

    #153 add bg service

---
+++ Market/OrderCancelService.cs
@@ -0,0 +1,78 @@
+﻿using General.Storage;
+
+using Market.Logic;
+using Market.Logic.Models;
+
+namespace WalletTransaction;
+
+/// <summary xml:lang = "ru">
+/// Фоновый сервис, занимающийся обработкой протухания неоплаченных заказов.
+/// </summary>
+public sealed class OrderCancelService : BackgroundService
+{
+    private readonly ILogger<OrderCancelService> _logger;
+    private readonly IHostApplicationLifetime _applicationLifetime;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+    private readonly TimeSpan _delayTime = TimeSpan.FromMinutes(1);
+    private readonly TimeSpan _timeoutAwait = TimeSpan.FromMinutes(15);
+
+    /// <summary xml:lang = "ru">
+    /// Создает новый объект типа <see cref="OrderCancelService"/>.
+    /// </summary>
+    /// <param name="logger" xml:lang = "ru">
+    /// Логгер.
+    /// </param>
+    /// <param name="applicationLifetime" xml:lang = "ru">
+    /// Жизненный цикл приложения.
+    /// </param>
+    /// <param name="serviceScopeFactory" xml:lang = "ru">
+    /// Фабрика служб.
+    /// </param>
+    /// <exception cref="ArgumentNullException" xml:lang = "ru">
+    /// Если любой из входных параметров оказался <see langword="null"/>.
+    /// </exception>
+    public OrderCancelService(
+        ILogger<OrderCancelService> logger,
+        IHostApplicationLifetime applicationLifetime,
+        IServiceScopeFactory serviceScopeFactory)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _applicationLifetime = applicationLifetime ?? throw new ArgumentNullException(nameof(applicationLifetime));
+        _serviceScopeFactory = serviceScopeFactory ?? throw new ArgumentNullException(nameof(serviceScopeFactory));
+    }
+
+    /// <inheritdoc/>
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(_delayTime);
+
+            _logger.LogDebug("Starting service...");
+
+            using var scope = _serviceScopeFactory.CreateScope();
+
+            var orderRepository = scope.ServiceProvider.GetRequiredService<IKeyableRepository<Order, ID>>();
+
+            var checkableOrder = orderRepository.GetEntities()
+                .Where(x => x.State is OrderState.PaymentWait);
+
+            foreach (var order in checkableOrder)
+            {
+                if (DateTime.Now > order.OrderDate + _timeoutAwait)
+                {
+                    _logger.LogInformation(
+                        "Order id: {OrderId} exceeded the payment waiting limit and will be closed",
+                        order.Key);
+
+                    order.State = OrderState.Cancel;
+                    // Todo: update order date
+                }
+            }
+
+            _logger.LogDebug("Stopping service...");
+        }
+
+        _applicationLifetime.StopApplication();
+    }
+}
