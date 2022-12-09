9d263c633cab7c8cfde47e327ef29540a82c2c2a
w1sp <semen2002bs@gmail.com>
2022-12-09 08:11:50 +0300

    #111 add provider controller

---
+++ Market/Controllers/ProviderController.cs
@@ -0,0 +1,41 @@
+﻿using General.Storage;
+
+using Market.Logic;
+using Market.Logic.Models;
+using Market.Logic.Storage.Repositories;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Market.Controllers;
+
+[Authorize]
+public class ProviderController : Controller
+{
+    private readonly IKeyableRepository<Provider, ID> _providerRepository;
+    private readonly IUsersRepository _usersRepository;
+    private readonly ILogger<ProviderController> _logger;
+    public ProviderController(IKeyableRepository<Provider, ID> providerRepository,
+        IUsersRepository usersRepository,
+        ILogger<ProviderController> logger)
+    {
+        _providerRepository = providerRepository;
+        _usersRepository = usersRepository;
+        _logger = logger;
+    }
+
+    [HttpGet("/newprovider")]
+    public IActionResult Create() => View();
+
+    [HttpPost("/newprovider")]
+    public IActionResult Create()
+    {
+
+    }
+
+    [HttpGet]
+    public IActionResult Providers() => View();
+
+    [HttpGet]
+    public IActionResult Provider() => View();
+}
