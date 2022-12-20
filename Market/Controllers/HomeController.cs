c26275d215fe6725cda0bd24a55c7dc50cb5f33f
w1sp <semen2002bs@gmail.com>
2022-12-20 22:59:29 +0300

    #147 setup home page

--- Market/Controllers/HomeController.cs
+++ Market/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics;
 
+using Market.Logic.Models;
+using Market.Logic.Models.Abstractions;
 using Market.Logic.Storage;
+using Market.Logic.Storage.Repositories;
 using Market.Models;
 
 using Microsoft.AspNetCore.Authorization;
@@ -13,15 +16,37 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly MarketContext _context;
+    private readonly IUsersRepository _usersRepository;
+    private readonly ICatalog _catalog;
 
-    public HomeController(ILogger<HomeController> logger, MarketContext context)
+    public HomeController(
+        ICatalog catalog, 
+        IUsersRepository usersRepository, 
+        ILogger<HomeController> logger, 
+        MarketContext context)
     {
         _context = context;
         _logger = logger;
+        _catalog = catalog;
+        _usersRepository = usersRepository;
     }
 
     [HttpGet]
-    public IActionResult Index() => View();
+    public IActionResult Index()
+    {
+        var user = GetCurrentUser();
+
+        var viewModel = new HomeIndexViewModel();
+
+        if (user is not null)
+        {
+            viewModel.LoginUserType = user.Type;
+        }
+
+        viewModel.Types = _catalog.GetItemTypes().Take(4);
+
+        return View(viewModel);
+    }
 
     [HttpGet]
     public IActionResult Privacy() => View();
@@ -35,4 +60,12 @@ public class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error() =>
         View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+    private User? GetCurrentUser()
+    {
+        if (User.Identity is null || User.Identity.Name is null)
+            return null;
+
+        return _usersRepository.GetByEmail(User.Identity.Name);
+    }
 }
---
+++ Market/Models/HomeIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Market.Logic.Models;
+
+namespace Market.Models;
+
+public class HomeIndexViewModel
+{
+    public IEnumerable<ItemType> Types { get; set; }
+
+    public string? SearchString { get; set; }
+
+    public UserType LoginUserType { get; set; }
+}
