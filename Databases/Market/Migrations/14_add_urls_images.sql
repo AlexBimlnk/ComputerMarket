d185a38ca5d0727a7487b3040933c9c56190464c
w1sp <semen2002bs@gmail.com>
2022-12-20 18:08:46 +0300

    #178 add image in db

---
+++ Databases/Market/Migrations/14_add_urls_images.sql
@@ -0,0 +1,13 @@
+BEGIN;
+ALTER TABLE item_type
+DROP COLUMN IF EXISTS url_image;
+
+ALTER TABLE item_type
+ADD COLUMN url_image TEXT NULL;
+
+ALTER TABLE items
+DROP COLUMN IF EXISTS url_image;
+
+ALTER TABLE items
+ADD COLUMN url_image TEXT NULL;
+COMMIT;
