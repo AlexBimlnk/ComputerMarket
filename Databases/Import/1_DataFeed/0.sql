9e2ce908b854d9a583c4a7d0f3817dc8af9ae9dc
w1sp <semen2002bs@gmail.com>
2023-12-04 00:32:57 +0300

    #189 fix import datafeed script

--- Databases/Import/1_DataFeed/0.sql
+++ Databases/Import/1_DataFeed/0.sql
@@ -5,5 +5,5 @@ VALUES ('Ivanov');
 INSERT INTO links(internal_id, external_id, provider_id)
 VALUES (1, 2, 1);
 
-INSERT INTO histories(external_id, provider_id, product_name)
+INSERT INTO histories(external_id, provider_id, product_metadata)
 VALUES (2, 1, 'Intel Pentium G4560 LGA 1151');
